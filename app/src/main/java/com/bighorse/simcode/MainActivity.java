cdcb6e4693be04b19f0e62122e1ac0f295019f99
Gheeroppa <federico.ghir@gmail.com>
2018-10-27 11:50:18 +0100

    add AdMob init

--- app/src/main/java/com/bighorse/simcode/MainActivity.java
+++ app/src/main/java/com/bighorse/simcode/MainActivity.java
@@ -4,8 +4,12 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AdView mAdView;
 
     private ViewPager mViewPager;
     private MyPagerAdapter mMyPagerAdapter;
@@ -19,5 +23,9 @@ public class MainActivity extends AppCompatActivity {
         mViewPager.setAdapter(mMyPagerAdapter);
 
         MobileAds.initialize(this, "ca-app-pub-5005687032079051~5324024505");
+
+        mAdView = findViewById(R.id.adView_banner);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
 }
