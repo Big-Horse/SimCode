5ef66a2bbee1dfde521f02f7af84574a0909ef1d
Gheeroppa <federico.ghir@gmail.com>
2018-10-27 12:30:07 +0100

    add test id banner

--- app/src/main/java/com/bighorse/simcode/MainActivity.java
+++ app/src/main/java/com/bighorse/simcode/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mViewPager = (ViewPager)findViewById(R.id.pager);
         mMyPagerAdapter = new MyPagerAdapter(this);
-        mViewPager.setAdapter(mMyPagerAdapter);
+         mViewPager.setAdapter(mMyPagerAdapter);
 
         MobileAds.initialize(this, "ca-app-pub-5005687032079051~5324024505");
 
